0ae8abf858e559afad76646533b0915962be697e
Matthew Long <long.matthew29@gmail.com>
2018-08-11 17:27:04 -0400

    [updated] gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,89 +1,51 @@
-const _ = require('lodash');
-const path = require('path');
-const { createFilePath } = require('gatsby-source-filesystem');
+const _ = require("lodash")
+const Promise = require("bluebird")
+const path = require("path")
+const select = require(`unist-util-select`)
+const fs = require(`fs-extra`)
 
-exports.createPages = ({ boundActionCreators, graphql }) => {
-  const { createPage } = boundActionCreators;
+exports.createPages = ({ graphql, boundActionCreators }) => {
+  const { createPage } = boundActionCreators
 
-  return graphql(`
-    {
-      allMarkdownRemark(limit: 1000) {
-        edges {
-          node {
-            id
-            fields {
-              slug
+  return new Promise((resolve, reject) => {
+    const pages = []
+    const blogPost = path.resolve("./src/templates/blog-post.js")
+    resolve(
+      graphql(
+        `
+      {
+        allMarkdownRemark(limit: 1000) {
+          edges {
+            node {
+              frontmatter {
+                path
+              }
             }
-            frontmatter {
-              tags
-              templateKey
+          }
+        }
+        allFile(filter: { extension: { eq: "jpg" } }) {
+          edges {
+            node {
+              publicURL
             }
           }
         }
       }
-    }
-  `).then(result => {
-    if (result.errors) {
-      result.errors.forEach(e => console.error(e.toString()))
-      return Promise.reject(result.errors)
-    }
+    `
+      ).then(result => {
+        if (result.errors) {
+          console.log(result.errors)
+          reject(result.errors)
+        }
 
-    const posts = result.data.allMarkdownRemark.edges;
-
-    posts.forEach(edge => {
-      const id = edge.node.id;
-      
-      createPage({
-        path: edge.node.fields.slug,
-        tags: edge.node.frontmatter.tags,
-        component: path.resolve(
-          `src/templates/${String(edge.node.frontmatter.templateKey)}.js`
-        ),
-        // additional data can be passed via context
-        context: {
-          id,
-        },
+        // Create blog posts pages
+        _.each(result.data.allMarkdownRemark.edges, edge => {
+          createPage({
+            path: edge.node.frontmatter.path,
+            component: blogPost
+          })
+        })
       })
-    })
-
-    // tag pages:
-    let tags = [];
-
-    // iterate through each post, putting all found tags into `tags`
-    posts.forEach(edge => {
-      if (_.get(edge, `node.frontmatter.tags`)) {
-        tags = tags.concat(edge.node.frontmatter.tags)
-      }
-    });
-
-    // eliminate duplicate tags
-    tags = _.uniq(tags);
-
-    // make tag pages
-    tags.forEach(tag => {
-      const tagPath = `/tags/${_.kebabCase(tag)}/`
-
-      createPage({
-        path: tagPath,
-        component: path.resolve(`src/templates/tags.js`),
-        context: {
-          tag,
-        },
-      })
-    });
-  });
-};
-
-exports.onCreateNode = ({ node, boundActionCreators, getNode }) => {
-  const { createNodeField } = boundActionCreators
-
-  if (node.internal.type === `MarkdownRemark`) {
-    const value = createFilePath({ node, getNode });
-
-    createNodeField({
-      name: `slug`,
-      node,
-      value,
-    });
-  };
-};
+    )
+  })
+}
