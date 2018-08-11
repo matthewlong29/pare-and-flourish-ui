50a197f670572ec34586b674ad7df64aad9ecf80
Matthew Long <long.matthew29@gmail.com>
2018-08-11 17:27:26 -0400

    [updated] gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,169 +1,51 @@
-const config = require('./meta/config');
-
-const pathPrefix = config.pathPrefix === '/' ? '' : config.pathPrefix;
-
 module.exports = {
   siteMetadata: {
-    title: config.siteTitle,
-    siteUrl: config.siteUrl,
-    rssMetadata: {
-      site_url: config.siteUrl + pathPrefix,
-      feed_url: config.siteUrl + pathPrefix + config.siteRss,
-      title: config.siteTitle,
-      description: config.siteDescription,
-      image_url: `${config.siteUrl + pathPrefix}/icons/icon-512x512.png`,
-      author: config.userName,
-      copyright: config.copyright,
-    },
+    title: "Pare and Flourish",
+    author: "",
+    description: "",
   },
+  pathPrefix: '/',
   plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sass',
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/src/pages`,
-        name: 'pages',
+        path: `${__dirname}/src/pages/Posts`,
+        name: "posts",
       },
     },
     {
-      resolve: `gatsby-plugin-sitemap`,
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
+      resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/src/img`,
-        name: 'images',
-      },
-    },
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
-    {
-      resolve: 'gatsby-transformer-remark',
-      options: {
-        plugins: [],
+        name: `images`,
+        path: `${__dirname}/src/assets/images`,
       },
     },
     {
-      resolve: 'gatsby-plugin-netlify-cms',
+      resolve: `gatsby-transformer-remark`,
       options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
-      },
-    },
-    `gatsby-plugin-offline`,
-    {
-      resolve: `gatsby-plugin-nprogress`,
-      options: {
-        // setting a color is optional.
-        color: config.themeColor,
-        // disable the loading spinner.
-        showSpinner: false,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-google-tagmanager`,
-      options: {
-        id: config.googleTagManagerID,
-        includeInDevelopment: false,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: config.siteTitle,
-        short_name: config.siteTitleAlt,
-        start_url: '/',
-        background_color: config.backgroundColor,
-        theme_color: config.themeColor,
-        display: 'standalone',
-        icons: [
+        plugins: [
           {
-            src: `/icons/icon-192x192.png`,
-            sizes: `192x192`,
-            type: `image/png`,
-          },
-          {
-            src: `/icons/icon-512x512.png`,
-            sizes: `512x512`,
-            type: `image/png`,
-          },
-        ],
-      },
-
-    },
-    {
-      resolve: 'gatsby-plugin-feed',
-      options: {
-        setup (ref) {
-          const ret = ref.query.site.siteMetadata.rssMetadata;
-          ret.allMarkdownRemark = ref.query.allMarkdownRemark;
-          ret.generator = 'Pare and Flourish';
-          return ret;
-        },
-        query: `
-                {
-                  site {
-                    siteMetadata {
-                      rssMetadata {
-                        site_url
-                        feed_url
-                        title
-                        description
-                        image_url
-                        author
-                        copyright
-                      }
-                    }
-                  }
-                }
-              `,
-        feeds: [
-          {
-            serialize (ctx) {
-              const rssMetadata = ctx.query.site.siteMetadata.rssMetadata
-              return ctx.query.allMarkdownRemark.edges
-                .filter(edge => edge.node.frontmatter.templateKey === 'article-page')
-                .map(edge => ({
-                  categories: edge.node.frontmatter.tags,
-                  date: edge.node.frontmatter.date,
-                  title: edge.node.frontmatter.title,
-                  description: edge.node.excerpt,
-                  author: rssMetadata.author,
-                  url: rssMetadata.site_url + edge.node.fields.slug,
-                  guid: rssMetadata.site_url + edge.node.fields.slug,
-                  custom_elements: [{'content:encoded': edge.node.html}],
-                }))
+            resolve: 'gatsby-remark-external-links',
+            options: {
+              target: '_blank',
+              rel: 'nofollow noopener noreferrer',
             },
-            query: `
-                    {
-                      allMarkdownRemark(
-                        limit: 1000,
-                        sort: { order: DESC, fields: [frontmatter___date] },
-                      ) {
-                        edges {
-                          node {
-                            excerpt(pruneLength: 400)
-                            html
-                            id
-                            fields { slug }
-                            frontmatter {
-                              title
-                              templateKey
-                              cover
-                              date(formatString: "MMMM DD, YYYY")
-                              tags
-                            }
-                          }
-                        }
-                      }
-                    }
-                  `,
-            output: config.siteRss,
           },
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 630,
+              quality: 90,
+              linkImagesToOriginal: false,
+            },
+          },
+          "gatsby-remark-copy-linked-files",
         ],
       },
     },
-    'gatsby-plugin-netlify',
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`
   ],
 }
-
