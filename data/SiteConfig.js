f0b4fcbce0164dee4be490bb38d6b09f5e86b90b
Matthew Long <long.matthew29@gmail.com>
2018-08-11 17:13:27 -0400

    [added] site config file

---
+++ data/SiteConfig.js
@@ -0,0 +1,17 @@
+module.exports = {
+  siteTitle: "Pare and Flourish", 
+  siteUrl: "", 
+  siteDescription: "", 
+  siteRss: "/rss.xml", 
+  siteFBAppID: "", 
+  googleAnalyticsID: "", 
+  dateFromFormat: "YYYY-MM-DD",
+  dateFormat: "DD/MM/YYYY",
+  Twitter: "https://twitter.com/pareandflourish/",
+  Facebook: "https://www.facebook.com/pareandflourish/",
+  GitHub:  "https://github.com/mtlong29/pare-and-flourish/",
+  Instagram: "https://www.instagram.com/pareandflourish/",
+  Pinterest: "https://www.pinterest.com/pareandflourish/",
+  PayPal:  "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=5JHZTSRZ52BZ6",
+  LinkedIn: "https://www.linkedin.com/company/pare-and-flourish",
+};
