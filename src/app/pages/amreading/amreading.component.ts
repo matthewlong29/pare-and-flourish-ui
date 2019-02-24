d2dd548aa206455844146fed06400179c0f1f570
Matthew Long <long.matthew29@gmail.com>
2019-02-23 21:00:32 -0500

    [added] initial amreading page

---
+++ src/app/pages/amreading/amreading.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-amreading',
+  templateUrl: './amreading.component.html',
+  styleUrls: ['./amreading.component.scss']
+})
+export class AmreadingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
