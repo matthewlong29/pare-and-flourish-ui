029a5babcabf7ad7131fe5b0d160edfd5028cd7b
Matthew Long <long.matthew29@gmail.com>
2019-02-24 12:19:01 -0500

    [added] class for blog meta data

---
+++ src/app/classes/BlogPostMetaData.ts
@@ -0,0 +1,9 @@
+export class BlogPostMetaData {
+  title: string;
+  excerpt: string;
+  date: Date;
+  URL: string;
+  authors: Array<string>;
+  teaserImageURI: string;
+  featureImageURI: string;
+}
